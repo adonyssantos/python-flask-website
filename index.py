57075569abacd2a13b2f34b10d22bd7f1626b59e
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-28 12:49:06 -0400

    Add layout and courses page

--- index.py
+++ index.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
