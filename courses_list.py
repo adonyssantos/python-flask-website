864674a6373d40d9824591a63c9109d4aea70d7f
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-05-02 19:25:29 -0400

    Add courses_list, course_details and error_handler

---
+++ courses_list.py
@@ -0,0 +1,111 @@
+# Template course
+# {
+#     "id": "",
+#     "name": "",
+#     "description": "",
+#     "large_description": "",
+#     "image": "",
+#     "duration": "",
+#     "screenshot_image": "",
+# }
+
+courses_list = [
+    {
+        "id":
+        "1",
+        "name":
+        "Course 1",
+        "description":
+        "The first course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+1",
+        "duration":
+        "3 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "2",
+        "name":
+        "Course 2",
+        "description":
+        "The second course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+2",
+        "duration":
+        "4 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "3",
+        "name":
+        "Course 3",
+        "description":
+        "The third course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+3",
+        "duration":
+        "5 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "4",
+        "name":
+        "Course 4",
+        "description":
+        "The fourth course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+4",
+        "duration":
+        "6 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "5",
+        "name":
+        "Course 5",
+        "description":
+        "The fifth course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+5",
+        "duration":
+        "7 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "6",
+        "name":
+        "Course 6",
+        "description":
+        "The sixth course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+6",
+        "duration":
+        "8 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+    {
+        "id":
+        "7",
+        "name":
+        "Course 7",
+        "description":
+        "The seventh course description",
+        "image":
+        "https://via.placeholder.com/468x300?text=Course+7",
+        "duration":
+        "9 months",
+        "screenshot_image":
+        "https://via.placeholder.com/1280x720?text=Course+Screenshot",
+    },
+]
--- index.py
+++ index.py
@@ -1,18 +1,38 @@
 from flask import Flask, render_template
+from courses_list import courses_list
 
 app = Flask(__name__)
+PORT = 5000
+DEBUG = True
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
+@app.route("/courses")
+def courses():
+    return render_template("courses_list.html", courses_list=courses_list)
+
+
+@app.route("/courses/<id>")
+def course(id):
+    for course in courses_list:
+        if course["id"] == id:
+            return render_template("course_details.html", course=course)
+    return render_template("404.html")
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/courses")
-def courses():
-    return render_template("courses.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=PORT, debug=DEBUG)
